6b9987216f96e82cb1c218aa3a328d5ec2cac40f
Brais <bvirlan@cifpfbmoll.eu>
2022-02-16 16:58:30 +0100

    Coger datos de la Base de Datos

--- PruebaAPI/app/Http/Controllers/ApiController.php
+++ PruebaAPI/app/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ApiController extends Controller
 {
@@ -13,7 +14,8 @@ class ApiController extends Controller
      */
     public function index()
     {
-        return "Prueba";
+        $paises = DB::table('paises')->get();
+        return $paises;
     }
 
     /**
--- PruebaAPI/public/apirest.js
+++ PruebaAPI/public/apirest.js
@@ -3,15 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
 
-function api_js_index(){
+function api_js_index() {
     const xhttp = new XMLHttpRequest();
-    xhttp.onload = function (){
-    document.getElementById('resultado').innerHTML = xhttp.responseText;
-};
-xhttp.open('GET', 'index.php/api/apirest', true);
+    xhttp.onload = function () {
+       /* var respuesta = xhttp.responseText;
+        respuest = JSON.parse(respuesta);
+        //respuesta.forEach(crearElementos);*/
+        
+        document.getElementById('resultado').innerHTML = xhttp.responseText;
+    };
+    xhttp.open('GET', 'index.php/api/apirest', true);
 //xhttp.setRequestHeader('Content-Type', 'application/vnd.api+json')
 //xhttp.setRequestHeader('Accept', 'application/vnd.api+json')
-xhttp.send();
+    xhttp.send();
 
 }
 
+
+function crearElementos(item) {
+    
+    var pais = JSON.parse(item)
+    
+}
